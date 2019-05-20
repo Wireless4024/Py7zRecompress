c12ff9c971c233f534c059b77e2c19360503545e
Wireless4024 <36229606+Wireless4024@users.noreply.github.com>
2019-05-20 19:12:38 +0700

    add parameter to include/exclude extension

--- py7zRecompress.py
+++ py7zRecompress.py
@@ -10,18 +10,17 @@ import threading
 import time
 
 _7zCompressable = ('.7z', '.xz', '.zip', '.tar', '.jar')
-_7zCompressArgs = '"{_7z}" a -sdel -mx9 -ssw -mmt{thread} -myx9 -md{dictsz} -aoa -mfb273 ' \
+_7zCompressArgs = '"{_7z}" a -sdel -mx9 -ssw -mmt{thread} -myx9 -md{dictsz}m -aoa -mfb273 ' \
 				  '-ms=on "{dest}" "{src}"'
-_xzCompressArgs = '"{_7z}" a -sdel -txz -mx9 -ssw -mmt{thread} -myx9 -md{dictsz} -aoa -mfb273 ' \
+_xzCompressArgs = '"{_7z}" a -sdel -txz -mx9 -ssw -mmt{thread} -myx9 -md{dictsz}m -aoa -mfb273 ' \
 				  '-ms=on "{dest}" "{src}"'
 _zipCompressArgs = '"{_7z}" a -sdel -tzip -mx9 -mfb258 -mmt{thread} -ssw -aoa "{dest}" "{src}"'
 _7zExtractArgs = '"{_7z}" x "{src}" "-o{dest}" -y -r'
 
 _default7z = "C:\\Program Files\\7-Zip\\7z.exe" if platform.system() == 'Windows' and os.path.exists(
 	"C:\\Program Files\\7-Zip\\7z.exe") else '7z'
-_defaultDictSize = '96m'
 
-gb = {}
+gb = {}  # gb = global
 
 
 def terminal_width():
@@ -29,10 +28,11 @@ def terminal_width():
 		term_width, void = os.get_terminal_size()
 	except Exception:
 		term_width = 80
-	return min(max(20, term_width), 120) - 3
+	return min(max(20, term_width), 120) - 4
 
 
-def compress(_7z = _default7z, source = '', dest = os.getcwd(), dictionary_size = _defaultDictSize, method = '7z'):
+def compress(_7z = _default7z, source = '', dest = os.getcwd(), dictionary_size = 96, method = '7z'):
+	exit(0)
 	if source == '':
 		return False
 	if method == 'xz':
@@ -90,13 +90,14 @@ class File:
 
 	@staticmethod
 	def deep_compress(folder):
-		execommand('"{}" "{}" "{}" -wt "{}" -ct "{}" -d "{}" -s "{}" -dc "{}" -7z "{}" -_deep "{}"'.format(
-			sys.executable, sys.argv[0], folder, gb['wt'], gb['ct'], gb['deep'] - 1, 'yes', gb['dict'], gb['7z'], True))
+		execommand('"%s" "%s" "%s" -wt %s -ct %s -d %s -s %s -dc %s -7z "%s" -_deep %s' % (
+			sys.executable, sys.argv[0], folder, gb['wt'], gb['ct'], gb['deep'] - 1, 'yes', gb['dict'], gb['7z'],
+			True))
 
 	# noinspection SpellCheckingInspection
 	def recompress(self):
-		print("\rWorking on [{}]".format(self))
-		temp = os.path.join(os.getcwd(), 'tmp', str(time.time()) + '-' + self.name.replace('.', '-'))
+		print("\rWorking on [%s]" % self)
+		temp = os.path.join(os.getcwd(), 'tmp', str(int(time.time())) + '-' + self.name.replace('.', '-'))
 		self.status = 'creating temp'
 		if not os.path.exists(temp):
 			os.mkdir(temp)
@@ -108,12 +109,12 @@ class File:
 			self.status = 'waiting deep compress'
 			self.deep_compress(temp)
 		self.status = 'compressing'
-		compress(source = temp, dest = self.out, method = self.type)
+		compress(source = temp, dictionary_size = gb['dict'], dest = self.out, method = self.type)
 		self.status = 'removing temp'
 		os.rmdir(temp)
 		self.status = 'done'
 		Manager.working.remove(self)
-		print("\r[ done ] {}".format(self, -9))
+		print("\r[done] %s" % self)
 
 	def __str__(self):
 		return self.path
@@ -122,19 +123,24 @@ class File:
 class Manager:
 	files = []
 	working = []
+	total = 0
 
 	def __init__(self, locations, sensitive = False, threads = 1):
 		self.run(threads)
-		length = 0
-		fileTypes = _7zCompressable if sensitive else (*_7zCompressable, '.rar', '.gz')
+		print(_7zCompressable)
+		file_types = _7zCompressable if sensitive else [*_7zCompressable, '.rar', '.gz']
+		print(file_types)
+		file_types = [x for x in (*file_types, *gb['include']) if not x.endswith((*gb['exclude'],))]
+		print(gb)
+		print(file_types)
 		for location in locations:
-			print("scanning directory '{}'".format(location))
+			print("scanning directory '%s'" % location)
 			temp = [File(os.path.join(root, f), sensitive)
 					for root, dirs, files in os.walk(location) for f
-					in files if f.endswith(fileTypes)]
-			length += len(temp)
+					in files if f.endswith((*file_types,))]
+			Manager.total += len(temp)
 			Manager.files.extend(temp)
-		print("Found {} files!".format(len(Manager.files)))
+		print("Found {} files!".format(Manager.total))
 		gb['finished'] = True
 
 	@staticmethod
@@ -162,12 +168,28 @@ class Work(threading.Thread):
 def print_status():
 	while len(Manager.files) < 1 and not gb['finished']:
 		time.sleep(1)
-	while len(Manager.working) > 0:
+	while len(Manager.working) > 0 or len(Manager.files) > 0:
 		for work in Manager.working:
-			print("\r[ {} ] {}".format(work.status, wrap(work, -len(work.status))), end = '')
-			time.sleep(1)
-		print("running [{}] task | remaining [{}] files".format(len(Manager.working), len(Manager.files)), end = '')
+			print("\r[%s] %s" % (work.status, wrap(work, -(len(work.status) + 3))), end = '')
+			time.sleep(2)
+
+		print("\rRunning [%s] task | Remaining [%s] files" % (len(Manager.working), len(Manager.files)), end = '')
 		time.sleep(2)
+		print(gen_prog(terminal_width()), end = '')
+		time.sleep(2)
+	print('\rDone!? | %s file has been re-compressed' % Manager.total)
+
+
+def gen_prog(leng):
+	total = Manager.total
+	remaining = len(Manager.files)
+	working = len(Manager.working)
+	done = total - remaining - working
+	str_rem = str(done) + '/' + str(total) + ' '
+	leng -= len(str_rem)
+	return "\r%s[%s%s%s]" % (str_rem, '#' * int((done / total) * leng),
+							 '-' * int((working / total) * leng),
+							 '.' * int((remaining / total) * leng))
 
 
 def wrap(msg, length = 0):
@@ -176,7 +198,7 @@ def wrap(msg, length = 0):
 	if length == 0:
 		length = terminal_width()
 	msg = str(msg)
-	return ('..' + msg[len(msg) - length:].strip(' \r\n\t')) if len(msg) > length else msg
+	return ('...' + msg[len(msg) - length:].strip(' \r\n\t')) if len(msg) > length else msg
 
 
 def deeper(path = ''):
@@ -185,14 +207,6 @@ def deeper(path = ''):
 			if f.endswith(_7zCompressable):
 				return True
 	return False
-
-
-# noinspection SpellCheckingInspection
-def argstr(arr):
-	base = ' '
-	for i in arr:
-		base += '"{}" '.format(i)
-	return base
 
 
 if __name__ == '__main__':
@@ -210,6 +224,10 @@ if __name__ == '__main__':
 					  help = 'dictionary size (megabytes)')
 	args.add_argument('-7z', '--executable', default = _default7z, dest = '_7z_path', type = str,
 					  help = '7zip executable path')
+	args.add_argument('-e', '--exclude', default = '', dest = 'exclude', type = str,
+					  help = 'excluded file extension separator is ; like "-e 7z;xz"')
+	args.add_argument('-i', '--include', default = '', dest = 'include', type = str,
+					  help = 'include file extension separator is ; like "-i iso;wim"')
 	args.add_argument('-_deep', default = False, dest = '_deep', type = bool,
 					  help = 'deep call from itself (internal use)')
 	arg = args.parse_args()
@@ -227,12 +245,20 @@ if __name__ == '__main__':
 	gb['ct'] = arg.compress_thread
 	gb['deep'] = 2147483647 if arg.deep_count < 0 else arg.deep_count
 	gb['sensitive'] = arg.s.startswith('y')
-	gb['dict'] = "{}m".format(arg.dictionary_size)
+	gb['dict'] = arg.dictionary_size
 	# noinspection PyProtectedMember
 	gb['7z'] = arg._7z_path
 	# noinspection PyProtectedMember
 	gb['deepcall'] = arg._deep
 	gb['finished'] = False
+	gb['exclude'] = arg.exclude.split(';')
+	gb['include'] = arg.include.split(';')
+	if len(gb['include'][0]) < 1:
+		gb['include'] = []
+	if len(gb['exclude'][0]) < 1:
+		gb['exclude'] = []
+	else:
+		gb['include'] = ['.' + i for i in gb['include']]
 	if not os.path.exists('tmp'):
 		os.mkdir('tmp')
 	if not os.path.exists('old'):
