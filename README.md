a4ed7262b97faaf929d40d0dfd364c69ed228c40
Wireless4024 <36229606+Wireless4024@users.noreply.github.com>
2019-05-24 22:56:38 +0700

    auto undo if error | new arguments

--- README.md
+++ README.md
@@ -13,13 +13,22 @@ Required software
 	> Windows : `> curl -o py7zRecompress.py https://raw.githubusercontent.com/Wireless4024/Py7zRecompress/master/py7zRecompress.py`
 
 3. Run! `$ python py7zRecompress.py <Folder> [Folders...] [-wt work_thread] [-ct compress_thread] [-d deep_count] [-s extension_sensitive] [-dc dictionary_size] [-7z 7zExecutable]`
-	> `Folder` : (`directory`) Folder that contains compress-able files `[7z, xz, zip, jar, rar, gz]`  
-	> `-wt work_thread` : (`int`) Working thread count (default : `1`)  
-	> `-ct compress_thread` : (`int`) Thread count used by 7zip (default : `3`)  
-	> `-d deep_count`   : (`int`) How deep to re-compress (useful when you have zip inside zip again and again) (default : `2`)  
-	> `-s extension_sensitive` : (`yes`|`no`) use compress algorithm same with file extension (default : `no`)  
-	> `-dc dictionary_size`    : (`int`) size of dictionary in MegaBytes used by `7z` and `xz` (default : `96`)  
-	> `-7z 7z_path`     : path to 7z executable (default : "`7z.exe`|`7z`")  
+	> `Folder path ...`          : Folder that contains compress-able files `[7z, xz, zip, jar, rar, gz]`  
+	> `-wt int`         : Working thread count (default : `1`)  
+	> `-ct int`         : Thread count used by 7zip (default : `3`)  
+	> `-d int`          : How deep to re-compress (useful when you have zip inside zip again and again) (default : `2`)  
+	> `-s yes|no`       : use compress algorithm same with file extension (default : `no`)  
+	> `-dc int`         : size of dictionary in MegaBytes used by `7z` and `xz` (default : `96`)  
+	> `-7z path`        : path to 7z executable (default : "`7z.exe`|`7z`")  
+	> `-e file_extension;...`   : excluded file extension eg. `-e 7z;rar` will not re-compress `.7z` and `.rar` file  
+	> `-i file_extension;...`   : include file extension eg. `-i iso;wim` will re-compress default file extensions and `.iso` and `.wim` file  
+	> `-o file_extension;...`   : re-compress only file extension eg. `-o zip` will re-compress only `.zip` file  
+	> `-n yes|no`       : do not backup  (default : `no`) 
+	> `-t int`          : extract timeout (default : `900` -> 15mins)  
+	> `-p string`       : file password (use when archive is locked)  
+	> `-ca string`      : additional argument when compress for 7zip  
+	> `-ea string`      : additional argument when extract for 7zip  
+	> `-skip yes|no`    : skip locked file (default : `yes` if password is empty)  
 	
 	> `-h` : show all help
 
@@ -44,8 +53,4 @@ Required software
 	### *Note*
 	> `Deep` parameter will very very slow if file/folder contains lots of file  
 
-	> you need to manually delete `old` folder. in case if 7zip crashed or program has accidental terminates your file will be here  
-
-	> please manually extract locked file before it will cause deadlock   
-	
 	> _**it will work even it's not efficient**_
--- py7zRecompress.py
+++ py7zRecompress.py
@@ -9,6 +9,7 @@ import subprocess
 import sys
 import threading
 import time
+from typing import *
 
 _7zCompressable = ('.7z', '.xz', '.zip', '.tar', '.jar')
 _7zCompressArgs = '"{_7z}" a -sdel -mx9 -ssw -mmt{thread} -myx9 -md{dictsz}m -aoa -mfb273 ' \
@@ -16,12 +17,12 @@ _7zCompressArgs = '"{_7z}" a -sdel -mx9 -ssw -mmt{thread} -myx9 -md{dictsz}m -ao
 _xzCompressArgs = '"{_7z}" a -sdel -txz -mx9 -ssw -mmt{thread} -myx9 -md{dictsz}m -aoa -mfb273 ' \
 				  '-ms=on "{dest}" "{src}"'
 _zipCompressArgs = '"{_7z}" a -sdel -tzip -mx9 -mfb258 -mmt{thread} -ssw -aoa "{dest}" "{src}"'
-_7zExtractArgs = '"{_7z}" x "{src}" "-o{dest}" -y -r'
+_7zExtractArgs = '"{_7z}" x "{src}" "-o{dest}" -p"{pw}" -y -r'
 
 _default7z = "C:\\Program Files\\7-Zip\\7z.exe" if platform.system() == 'Windows' and os.path.exists(
 	"C:\\Program Files\\7-Zip\\7z.exe") else '7z'
 
-gb = {}  # gb = global
+gb = {}  # type:Dict[Union[str,int,list,tuple]]
 
 
 def terminal_width():
@@ -32,33 +33,51 @@ def terminal_width():
 	return min(max(20, term_width), 120) - 4
 
 
-def compress(_7z = _default7z, source = '', dest = os.getcwd(), dictionary_size = 96, method = '7z'):
+def compress(_7z = _default7z, source = '', dest = os.getcwd(), dictionary_size = 96, method = '7z') -> bool:
 	if source == '':
 		return False
+	arg = gb['carguments'] if len(gb['carguments']) > 0 else ''
 	if method == 'xz':
 		command = _xzCompressArgs.format(_7z = _7z, src = os.path.join(source, '*'), dest = dest,
-										 dictsz = dictionary_size, thread = gb['ct'])
-		execommand(command)
+										 dictsz = dictionary_size, thread = gb['ct']) + arg
+		return execommand(command)
 	elif method == 'zip' or method == 'jar':
 		command = _zipCompressArgs.format(_7z = _7z, src = os.path.join(source, '*'), dest = dest,
-										  dictsz = dictionary_size, thread = gb['ct'])
-		execommand(command)
+										  dictsz = dictionary_size, thread = gb['ct']) + arg
+		return execommand(command)
 	else:
 		command = _7zCompressArgs.format(_7z = _7z, src = os.path.join(source, '*'), dest = dest,
-										 dictsz = dictionary_size, thread = gb['ct'])
-		execommand(command)
+										 dictsz = dictionary_size, thread = gb['ct']) + arg
+		return execommand(command)
 
 
-def extract(_7z = _default7z, source = '', dest = os.path.join(os.getcwd(), 'tmp')):
+def extract(_7z = _default7z, source = '', dest = os.path.join(os.getcwd(), 'tmp')) -> bool:
 	if source == '':
 		return False
-	command = _7zExtractArgs.format(_7z = _7z, src = source, dest = dest)
-	execommand(command)
+	command = _7zExtractArgs.format(_7z = _7z, src = source, dest = dest, pw = gb['password']) + (
+		gb['earguments'] if len(gb['earguments']) > 0 else '')
+	return execommand(command, gb['timeout'])
 
 
-# noinspection SpellCheckingInspection
-def execommand(command = ''):
-	subprocess.Popen(command, shell = True, stdout = subprocess.DEVNULL).wait()
+def test7z(file, pw = ''):
+	return not execommand(r'"%s" t -p"%s" "%s"' % (gb['7z'], pw, file), exitCode = 2)
+
+
+def execommand(command = '', timeout = 2147483647, exitCode = 0) -> True | False:
+	"""
+	Execute following command \n
+	:param exitCode: return true if exit-code equals this parameter
+	:param command: a command <br>
+	:param timeout: max running time <br>
+	:return: True if process finished properly or False if reached timeout or exit code it not zero
+	"""
+	try:
+		return exitCode == subprocess.call(command, shell = True,
+										   stdout = subprocess.DEVNULL,
+										   stderr = sys.stdout,
+										   timeout = timeout)
+	except subprocess.TimeoutExpired:
+		return False
 
 
 def get_extension(val = '', sensitive = False):
@@ -80,68 +99,120 @@ class File:
 		# noinspection PyTypeChecker
 		self.out = os.path.join(os.path.dirname(path),
 								(self.name.rsplit('.', 1)[0] + '.' + self.type) if not sensitive else self.name)
+
+		self.old = os.path.join(os.getcwd(), 'old', str(int(time.time())) + '-' + self.name)
 		self.path = path
 		self.status = 'idle'
 
 	def backup(self):
-		old = os.path.join(os.getcwd(), 'old', str(int(time.time())) + '-' + self.name)
 		try:
-			os.rename(self.path, old)
+			os.rename(self.path, self.old)
 		except OSError:
-			shutil.move(self.path, old)
-		return old
+			shutil.move(self.path, self.old)
+		return self.old
 
 	@staticmethod
 	def deep_compress(folder):
-		execommand('"%s" "%s" "%s" -wt %s -ct %s -d %s -s %s -dc %s -7z "%s" -_deep %s' % (
+		return execommand('"%s" "%s" "%s" -wt %s -ct %s -d %s -s %s -dc %s -7z "%s" -n yes -_deep %s' % (
 			sys.executable, sys.argv[0], folder, gb['wt'], gb['ct'], gb['deep'] - 1, 'yes', gb['dict'], gb['7z'],
 			True))
 
+	def undo(self):
+		try:
+			os.rename(self.old, self.path)
+		except OSError:
+			shutil.move(self.old, self.path)
+
 	# noinspection SpellCheckingInspection
 	def recompress(self):
-		cll()
-		print("\rWorking on [%s]" % self)
-		temp = os.path.join(os.getcwd(), 'tmp', str(int(time.time())) + '-' + self.name.replace('.', '-'))
-		self.status = 'creating temp'
-		if not os.path.exists(temp):
-			os.mkdir(temp)
-		self.status = 'backing up'
-		backup = self.backup()
-		self.status = 'extracting'
-		extract(source = backup, dest = temp)
-		if gb['deep'] > 1 and deeper(temp):
-			self.status = 'waiting deep compress'
-			self.deep_compress(temp)
-		self.status = 'compressing'
-		compress(source = temp, dictionary_size = gb['dict'], dest = self.out, method = self.type)
-		self.status = 'removing temp'
-		os.rmdir(temp)
-		self.status = 'done'
-		Manager.working.remove(self)
-		cll()
-		print("\r[done] %s" % self)
+		if not test7z(self.path) and gb['skip']:
+			print('[Skip] %s cause Locked' % self.path)
+			return None
+		try:
+			cll()
+			print("\rWorking on [%s]" % self)
+			temp = os.path.join(os.getcwd(), 'tmp', str(int(time.time())) + '-' + self.name.replace('.', '-'))
+			self.status = 'creating temp'
+			if not os.path.exists(temp):
+				os.mkdir(temp)
+			self.status = 'backing up'
+			backup = self.backup()
+			self.status = 'extracting'
+			if not extract(source = backup, dest = temp):
+				cll()
+				raise Error.ExtractError("\r[Error] when extract archive %s\n" % self)
+			cll()
+			stats = DirStats(temp)
+			print("\rFound [%s] files | [%s] directories | size %0.6f Mbytes in [%s]" % (
+				stats.stats['file'], stats.stats['folder'], (stats.stats['size'] / 1024.0) / 1024.0, temp))
+			if gb['deep'] > 1 and deeper(temp):
+				self.status = 'waiting deep compress'
+				if not self.deep_compress(temp):
+					cll()
+					sys.stderr.write("\r[Error] when digging in archive %s\n" % self)
+
+			self.status = 'compressing'
+			if not compress(source = temp, dictionary_size = gb['dict'], dest = self.out, method = self.type):
+				raise Error.CompressError("\r[Error] when compressing %s\n" % self)
+			self.status = 'removing temp'
+			try:
+				os.rmdir(temp)
+			except OSError:
+				shutil.rmtree(temp)
+			self.status = 'done'
+			Manager.working.remove(self)
+			if not gb['no_keep']:
+				saveInfo(self.path, self.old)
+			else:
+				os.remove(self.old)
+			cll()
+			print("\r[done] %s" % self)
+		except Exception as e:
+			sys.stderr.write("\r[Error] %s | [Cause] %s\n" % (self, e.args[0]))
+			self.undo()
+			sys.stderr.write("\r[Info] %s undo successfully!\n" % self)
 
 	def __str__(self):
 		return self.path
 
 
+class Error:
+	class ExtractError(IOError):
+		def __init__(self, msg = ''):
+			self.msg = msg
+
+		def __str__(self):
+			return self.msg
+
+	class CompressError(IOError):
+		def __init__(self, msg = ''):
+			self.msg = msg
+
+		def __str__(self):
+			return self.msg
+
+
 class Manager:
-	files = []
-	working = []
+	files = []  # type: List[File,...]
+	working = []  # type: List[File,...]
 	total = 0
 
 	def __init__(self, locations, sensitive = False, threads = 1):
 		self.run(threads)
-		file_types = _7zCompressable if sensitive else [*_7zCompressable, '.rar', '.gz']
-		file_types = [x for x in (*file_types, *gb['include']) if not x.endswith((*gb['exclude'],))]
+		if len(gb['only']) < 1:
+			file_types = _7zCompressable if sensitive else (*_7zCompressable, '.rar', '.gz')
+			if len(gb['exclude']) > 0:
+				file_types = (x for x in (*file_types, *gb['include']) if not x.endswith((*gb['exclude'],)))
+		else:
+			file_types = (*gb['only'],)
 		for location in locations:
-			print("scanning directory '%s'" % location)
+			print("Scanning directory '%s'" % location)
 			temp = [File(os.path.join(root, f), sensitive)
 					for root, dirs, files in os.walk(location) for f
 					in files if f.endswith((*file_types,))]
 			Manager.total += len(temp)
 			Manager.files.extend(temp)
-		print("Found {} files!".format(Manager.total))
+		print("Found %s files!" % Manager.total)
 		gb['finished'] = True
 
 	@staticmethod
@@ -166,9 +237,39 @@ class Work(threading.Thread):
 			Manager.pop().recompress()
 
 
+class DirStats:
+	def __init__(self, directory):
+		self.dir = directory
+		self.stats = {'size': 0, 'folder': 0, 'file': 0}  # type: Dict[str:int]
+		self.walk()
+
+	def walk(self, path = None):
+		if path is None:
+			path = self.dir
+		for f in os.listdir(path):
+			f = os.path.join(path, f)
+			if os.path.isdir(f):
+				self.stats['folder'] += 1
+				self.walk(f)
+			elif os.path.isfile(f):
+				self.stats['size'] += os.stat(f).st_size
+				self.stats['file'] += 1
+
+	def __str__(self):
+		return "%s folder | %s files | size %s MBytes" % (
+			self.stats['folder'], self.stats['file'], ((self.stats['size'] / 1024.0) / 1024.0))
+
+
 # clear line
 def cll():
 	print('\r', ' ' * (terminal_width() + 3), end = '', sep = '')
+
+
+def saveInfo(path, old):
+	file = open('info.txt', 'a+')
+	file.write('[OLD] %s -> [ORIGINAL] %s' % (old, path))
+	file.newlines()
+	file.close()
 
 
 def print_status():
@@ -186,6 +287,7 @@ def print_status():
 		time.sleep(2)
 	cll()
 	print('\rDone!? | %s file has been re-compressed' % Manager.total)
+	exit(0)
 
 
 def gen_prog(leng):
@@ -195,9 +297,9 @@ def gen_prog(leng):
 	done = total - remaining - working
 	str_rem = str(done) + '/' + str(total) + ' '
 	leng -= len(str_rem)
-	return "\r%s[%s%s%s]" % (str_rem, '#' * int((done / total) * leng),
-							 '-' * int((working / total) * leng),
-							 '.' * int((remaining / total) * leng))
+	return "\r%s[%s%s%s]" % (str_rem, '|' * int((done / total) * leng),
+							 ':' * int((working / total) * leng),
+							 '-' * int((remaining / total) * leng))
 
 
 def wrap(msg, length = 0):
@@ -236,6 +338,20 @@ if __name__ == '__main__':
 					  help = 'excluded file extension separator is ; like "-e 7z;xz"')
 	args.add_argument('-i', '--include', default = '', dest = 'include', type = str,
 					  help = 'include file extension separator is ; like "-i iso;wim"')
+	args.add_argument('-o', '--only', default = '', dest = 'only', type = str,
+					  help = 're-compress only file extension separator is ; like "-o zip"')
+	args.add_argument('-n', '--no-keep', default = 'no', dest = 'no_keep', type = str,
+					  choices = ('y', 'yes', 'n', 'no'),  help = 'if yes will keep old file')
+	args.add_argument('-t', '--timeout', default = 900, dest = 'timeout', type = int,
+					  help = 'timeout in second if extract time is longer than the specified time program will abort this file')
+	args.add_argument('-p', '--password', default = '', dest = 'password', type = str,
+					  help = 'password to extract archive file')
+	args.add_argument('-ca', '--compress-arguments', default = '', dest = 'compress_arguments', type = str,
+					  help = 'additional argument when compress for 7zip')
+	args.add_argument('-ea', '--extract-arguments', default = '', dest = 'extract_arguments', type = str,
+					  help = 'additional argument when extract for 7zip')
+	args.add_argument('-skip', '--skip-locked', default = '', dest = 'skip', type = str,
+					  choices = ('y', 'yes', 'n', 'no'), help = 'skip locked file')
 	args.add_argument('-_deep', default = False, dest = '_deep', type = bool,
 					  help = 'deep call from itself (internal use)')
 	arg = args.parse_args()
@@ -261,16 +377,25 @@ if __name__ == '__main__':
 	gb['finished'] = False
 	gb['exclude'] = arg.exclude.split(';')
 	gb['include'] = arg.include.split(';')
+	gb['only'] = arg.only.split(';')
+	gb['no_keep'] = arg.no_keep.startswith('y')
+	gb['timeout'] = arg.timeout
+	gb['carguments'] = arg.compress_arguments
+	gb['earguments'] = arg.extract_arguments
+	gb['password'] = arg.password
+	gb['skip'] = arg.password == '' or arg.skip.startswith('y')
 	if len(gb['include'][0]) < 1:
 		gb['include'] = []
 	if len(gb['exclude'][0]) < 1:
 		gb['exclude'] = []
-	else:
-		gb['include'] = ['.' + i for i in gb['include']]
+	if len(gb['only'][0]) < 1:
+		gb['only'] = []
+
+	gb['include'] = ['.' + i if not i.startswith('.') else i for i in gb['include']]
+	gb['only'] = ['.' + i if not i.startswith('.') else i for i in gb['only']]
 	if not os.path.exists('tmp'):
 		os.mkdir('tmp')
 	if not os.path.exists('old'):
 		os.mkdir('old')
-
 	_thread.start_new_thread(print_status, ())
 	Manager(gb['path'], gb['sensitive'], gb['wt'])
